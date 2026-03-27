98c8d03acbc9b42fa94b0244d8108e332e9afb66
neonrecordsmusic-sketch <neonrecords.music@gmail.com>
2026-03-27 19:51:02 -0300

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// Troque 'thumb-studio' pelo nome exato do seu repositório no GitHub
-export default defineConfig({
-  plugins: [react()],
-  base: '/thumb-studio/',
-})
