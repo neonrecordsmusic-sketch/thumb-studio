4035564be63a813a3d42e35372a6ce5d5089ddc2
neonrecordsmusic-sketch <neonrecords.music@gmail.com>
2026-03-27 20:51:04 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import ThumbStudio from './ThumbStudio.jsx'
+import ThumbStudio from './thumbstudio.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
